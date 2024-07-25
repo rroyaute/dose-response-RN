aa8635f7c59135ed968dc8ea0e9f64d675513291
Raphael Royaute <raphael.royaute@inrae.fr>
2024-07-25 17:22:54 +0200

    Prior predictive checks

--- R/sims_DR_RN.R
+++ R/sims_DR_RN.R
@@ -1,6 +1,7 @@
 # Code for Dose-Response simulations incorporating VI
-library(tidyverse); library(viridis)
-
+library(tidyverse); library(viridis); library(brms); library(tidybayes)
+library(patchwork)
+# 1. Simulation on the Normal scale ----
 # Parameter list
 alpha = 100
 beta = .08
@@ -32,7 +33,7 @@ df %>%
   labs(x = "Dose", y = "y response") +
   theme_bw(14)
 
-# log version
+# 2. Simulations using a lognormal ikelihood ----
 DR_fun_log = function(Dose, alpha , beta, NEC){
   log_yhat = ifelse(Dose < NEC, log(alpha), 
                     log(alpha) - beta * (Dose - NEC))
@@ -66,7 +67,7 @@ df %>%
   labs(x = "Dose", y = "y response") +
   theme_bw(14)
 
-# Simulate individuals with different sensitivities
+#  3. Simulate individuals with different sensitivities ----
 Dose = seq(0, 100, by = 10)
 n_id = 8
 rho = 0 # Suppose all parameters are independent
@@ -123,8 +124,107 @@ fig = df %>%
                             beta = beta, 
                             NEC = NEC),
                 color = "red", size = 3) +
-  scale_color_viridis_d(guide = guide_legend(title = "ID (ordered by alpha_i)")) +
+  scale_color_viridis_d() +
   labs(x = "Dose", y = "y response") +
   theme_bw(14) +
   theme(legend.position = "none")
 fig
+
+
+# 4. Fit the model to brms
+bf.pop = bf(y ~ log(alpha) - beta * (Dose - NEC) * step((Dose - NEC)), 
+            alpha + beta + NEC ~ 1, 
+            nl = T, 
+            family = lognormal)
+bf.vi = bf(y ~ log(alpha) - beta * (Dose - NEC) * step((Dose - NEC)), 
+        alpha + beta + NEC ~ 1 + (1|c|ID), 
+        nl = T, 
+        family = lognormal)
+priors = get_prior(bf.vi, df)
+
+priors.pop = 
+  # Intercept priors
+  prior(normal(100, 20), nlpar = alpha, class = b, lb = 0) +
+  prior(normal(0, 1), nlpar = beta, class = b) +
+  prior(uniform(0, 100), nlpar = NEC, class = b, lb = 0, ub = 100) + 
+  # # Residual prior
+  prior(exponential(1), class = sigma)
+
+priors.vi = 
+  # Intercept priors
+  prior(normal(100, 20), nlpar = alpha, class = b, lb = 0) +
+  prior(normal(0, 1), nlpar = beta, class = b) +
+  prior(uniform(0, 100), nlpar = NEC, class = b, lb = 0, ub = 100) + 
+  # Random effects priors (informative priors with 20 % CV)
+  prior(exponential(1), nlpar = alpha, class = sd, group = ID) +
+  prior(exponential(1), nlpar = beta, class = sd, group = ID) +
+  prior(exponential(1), nlpar = NEC, class = sd, group = ID) +
+  # # Residual prior
+  prior(exponential(1), class = sigma) +
+  prior(lkj(4), class = cor)
+
+# Plot priors
+p1 = priors %>% 
+  parse_dist() %>% 
+  filter(class == "b") %>% 
+  ggplot(aes(xdist = .dist_obj, y = format(.dist_obj))) +
+  stat_dist_halfeye() +
+  facet_wrap(~nlpar, scales = "free") +
+  ggtitle("Intercepts") +
+  xlab("Value") + ylab("Density") +
+  theme_bw(12) +
+  theme(axis.text.y = element_text(angle = 90)) 
+
+p2 = priors %>% 
+  parse_dist() %>% 
+  filter(class == "sd") %>% 
+  ggplot(aes(xdist = .dist_obj, y = format(.dist_obj))) +
+  stat_dist_halfeye() +
+  facet_wrap(~nlpar, scales = "free") +
+  ggtitle("Among-individual variance") +
+  xlab("Value") + ylab("Density") +
+  theme_bw(12) +
+  theme(axis.text.y = element_text(angle = 90)) 
+
+p3 = priors %>% 
+  parse_dist() %>% 
+  filter(class == "sigma") %>% 
+  ggplot(aes(xdist = .dist_obj, y = format(.dist_obj))) +
+  stat_dist_halfeye() +
+  facet_wrap(~nlpar, scales = "free") +
+  ggtitle("Residual variance") +
+  xlab("Value") + ylab("Density") +
+  theme_bw(12) +
+  theme(axis.text.y = element_text(angle = 90)) 
+
+(p1 + p2 + p3) + plot_layout(ncol = 1)
+
+## 3.1 Prior predictive checks on the population model ----
+brm.pop.prior= brm(data = df, 
+                   bf.pop, 
+                   backend = "cmdstan",
+                   prior = priors.pop, 
+                   sample_prior = "only", 
+                   file_refit = "always",
+                   save_pars = save_pars(all = TRUE),
+                   seed = 42, 
+                   file = "mods/brm.pop.prior")
+
+brm.pop.prior
+color_scheme_set("darkgray")
+pp_check(brm.pop.prior, ndraws = 100)
+conditional_effects(brm.pop.prior)
+
+brm.pop= brm(data = df, 
+             bf.pop, 
+             backend = "cmdstan",
+             prior = priors.pop, 
+             sample_prior = "yes",
+             save_pars = save_pars(all = TRUE),
+             seed = 42, 
+             cores = 4,
+             threads = 3,
+             control = list(adapt_delta = .95,
+                            max_treedepth = 12), 
+             file = "mods/brm.pop")
+
