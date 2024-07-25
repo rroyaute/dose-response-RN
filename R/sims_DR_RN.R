cdefd2b10a64bbc3b77143fdcd6411980c4fd78c
Raphael Royaute <raphael.royaute@inrae.fr>
2024-07-25 15:10:31 +0200

    Update sims_DR_RN.R
    Convert to lognormal scale

--- R/sims_DR_RN.R
+++ R/sims_DR_RN.R
@@ -3,8 +3,8 @@ library(tidyverse)
 
 # Parameter list
 alpha = 100
-beta = .8
-Dose = seq(0, 100, by = 10)
+beta = .08
+Dose = seq(0, 100, by = 1)
 NEC = 25
 sigma = 1
 nreps = 10
@@ -29,5 +29,39 @@ df %>%
                             NEC = NEC),
                 color = "red", size = 1) +
   ylim(0, 100) +
+  labs(x = "Dose", y = "y response") +
+  theme_bw(14)
+
+# log version
+DR_fun_log = function(Dose, alpha , beta, NEC){
+  log_yhat = ifelse(Dose < NEC, log(alpha), 
+                    log(alpha) - beta * (Dose - NEC))
+  return(log_yhat)
+}
+
+DR_fun_log_exp = function(Dose, alpha , beta, NEC){
+  log_yhat = ifelse(Dose < NEC, log(alpha), 
+                    log(alpha) - beta * (Dose - NEC))
+  yhat = exp(log_yhat)
+  return(yhat)
+}
+
+plot(Dose, exp(DR_fun_log(Dose, alpha , beta, NEC)))
+
+sigma = .05
+
+set.seed(42)
+df = crossing(rep = 1:nreps, Dose) %>% 
+  mutate(log_yhat = DR_fun_log(Dose, alpha, beta, NEC)) %>% 
+  mutate(y = rlnorm(n(), log_yhat, sigma))
+df %>% 
+  ggplot(aes(y = y, x = Dose)) +
+  geom_point(alpha = .2, size = 2.5) +
+  geom_function(fun = DR_fun_log_exp, 
+                args = list(alpha = alpha, 
+                            beta = beta, 
+                            NEC = NEC),
+                color = "red", size = 1) +
+  # ylim(0, 100) +
   labs(x = "Dose", y = "y response") +
   theme_bw(14)
