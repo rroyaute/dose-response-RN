56aedbb325964c302873865eadf466a21175aedf
Raphael Royaute <raphael.royaute@inrae.fr>
2025-10-21 09:24:51 +0200

    Delete SBC section for data simulations

--- R/sims_DR_RN_short.R
+++ R/sims_DR_RN_short.R
@@ -9,170 +9,7 @@ theme_set(theme_bw(14))
 
 source("R/funs/script_funs.R")
 
-# Simulate DGP ----
-I = 1000
-CVi = .5
-nsim = 100
-
-# Test case with extreme individual variation
-df = data_generator(ID_pars = "all", CVi = CVi, I = I)
-vars = df$variables
-df = df$generated 
-
-fig.all = df %>% 
-  ggplot(aes(y = y, x = Dose)) +
-  # Add individual trends
-  map(1:I, ~geom_function(
-    fun = DR_logRN_fun,
-    args = list(
-      # Dose = seq(0, 100, length.out = 200),
-      Rmin = log(exp(vars$b_Rmin_Intercept)),
-      Rmax = log(exp(vars$b_Rmax_Intercept + vars$r_ID__Rmax))[.x],
-      beta = (vars$b_beta_Intercept + vars$r_ID__beta_Intercept)[.x],
-      NEC =( vars$b_NEC_Intercept + vars$r_ID__NEC_Intercept)[.x]),
-    alpha = 0.2, 
-    linewidth = .25)) +
-  # Add population average trend
-  geom_function(fun = DR_logRN_fun, 
-                args = list(Rmin = log(exp(vars$b_Rmin_Intercept)), 
-                            Rmax = log(exp(vars$b_Rmax_Intercept)), 
-                            beta = vars$b_beta_Intercept, 
-                            NEC = vars$b_NEC_Intercept),
-                color = "red", linewidth = 1.25) +
-  labs(x = "Dose", y = "Response") +
-  theme_bw(14) +
-  theme(legend.position = "none") #+
-  # ggtitle("All parameters", 
-  #         subtitle = "with sd = 0.5")
-fig.all
-ggsave("outputs/figs/fig.all.jpeg",
-       fig.all, 
-       width = 18, 
-       scale = 1.2,
-       units = "cm")
-
-hist(exp(vars$b_Rmax_Intercept + vars$r_ID__Rmax))
-mu_Rmax = mean(exp(vars$b_Rmax_Intercept + vars$r_ID__Rmax))
-sd_Rmax = sd(exp(vars$b_Rmax_Intercept + vars$r_ID__Rmax))
-CV = sd_Rmax / mu_Rmax * 100
-CV
-
-hist(vars$b_NEC_Intercept + vars$r_ID__NEC)
-mu_NEC = mean(vars$b_NEC_Intercept + vars$r_ID__NEC)
-sd_NEC = sd(vars$b_NEC_Intercept + vars$r_ID__NEC)
-CV = sd_NEC / mu_NEC * 100
-CV
-
-hist(exp(vars$b_beta_Intercept + vars$r_ID__beta))
-mu_beta = mean(exp(vars$b_beta_Intercept + vars$r_ID__beta))
-sd_beta = sd(exp(vars$b_beta_Intercept + vars$r_ID__beta))
-CV = sd_beta / mu_beta * 100
-CV
-
-
-# Linear regression by dose class ----
-## Sample ID by dose pairs -----
-D_eps <- .00001 # for log-scale plotting
-df <- df %>% mutate(Dose = case_when(Dose == 0 ~ D_eps, .default = Dose))
-df.1 <- df %>% 
-  filter(ID %in% sample(unique(ID), 30)) %>% 
-  filter(Dose == D_eps | Dose == 10)
-df.2 <- df %>% 
-  filter(ID %in% sample(unique(ID), 30)) %>% 
-  filter(Dose == D_eps | Dose == 20)
-df.3 <- df %>% 
-  filter(ID %in% sample(unique(ID), 30)) %>% 
-  filter(Dose == D_eps | Dose == 30)
-df.4 <- df %>% 
-  filter(ID %in% sample(unique(ID), 30)) %>% 
-  filter(Dose == D_eps | Dose == 40)
-df.5 <- df %>% 
-  filter(ID %in% sample(unique(ID), 30)) %>% 
-  filter(Dose == D_eps | Dose == 50)
-df.6 <- df %>% 
-  filter(ID %in% sample(unique(ID), 30)) %>% 
-  filter(Dose == D_eps | Dose == 60)
-df.7 <- df %>% 
-  filter(ID %in% sample(unique(ID), 30)) %>% 
-  filter(Dose == D_eps | Dose == 70)
-df.8 <- df %>% 
-  filter(ID %in% sample(unique(ID), 30)) %>% 
-  filter(Dose == D_eps | Dose == 80)
-df.9 <- df %>% 
-  filter(ID %in% sample(unique(ID), 30)) %>% 
-  filter(Dose == D_eps | Dose == 90)
-df.10 <- df %>% 
-  filter(ID %in% sample(unique(ID), 30)) %>% 
-  filter(Dose == D_eps | Dose == 100)
-
-## plot ----
-p1 <- df.1 %>% 
-  ggplot(aes(y = y, x = Dose, group = ID)) +
-  geom_point(alpha = .2, size = 2.5) +
-  geom_line(alpha = .5) +
-  xlim(0, 100)
-p2 <- df.2 %>% 
-  ggplot(aes(y = y, x = Dose, group = ID)) +
-  geom_point(alpha = .2, size = 2.5) +
-  geom_line(alpha = .5) +
-  xlim(0, 100)
-p3 <- df.3 %>% 
-  ggplot(aes(y = y, x = Dose, group = ID)) +
-  geom_point(alpha = .2, size = 2.5) +
-  geom_line(alpha = .5) +
-  xlim(0, 100)
-p4 <- df.4 %>% 
-  ggplot(aes(y = y, x = Dose, group = ID)) +
-  geom_point(alpha = .2, size = 2.5) +
-  geom_line(alpha = .5) +
-  xlim(0, 100)
-p5 <- df.5 %>% 
-  ggplot(aes(y = y, x = Dose, group = ID)) +
-  geom_point(alpha = .2, size = 2.5) +
-  geom_line(alpha = .5) +
-  xlim(0, 100)
-p6 <- df.6 %>% 
-  ggplot(aes(y = y, x = Dose, group = ID)) +
-  geom_point(alpha = .2, size = 2.5) +
-  geom_line(alpha = .5) +
-  xlim(0, 100)
-p7 <- df.7 %>% 
-  ggplot(aes(y = y, x = Dose, group = ID)) +
-  geom_point(alpha = .2, size = 2.5) +
-  geom_line(alpha = .5) +
-  xlim(0, 100)
-p8 <- df.8 %>% 
-  ggplot(aes(y = y, x = Dose, group = ID)) +
-  geom_point(alpha = .2, size = 2.5) +
-  geom_line(alpha = .5) +
-  xlim(0, 100)
-p9 <- df.9 %>% 
-  ggplot(aes(y = y, x = Dose, group = ID)) +
-  geom_point(alpha = .2, size = 2.5) +
-  geom_line(alpha = .5) +
-  xlim(0, 100)
-p10 <- df.10 %>% 
-  ggplot(aes(y = y, x = Dose, group = ID)) +
-  geom_point(alpha = .2, size = 2.5) +
-  geom_line(alpha = .5) +
-  xlim(0, 100)
-
-## Stitch figure ----
-fig_lin_rn <- 
-  (p1 | p2 | p3 | p4 | p5) /
-  (p6 | p7 | p8 | p9 | p10) 
-
-ggsave("outputs/figs/fig_lin_rn.jpeg",
-       fig_lin_rn, 
-       width = 18, 
-       scale = 1.2,
-       units = "cm")
-  
-
-
-
-# Manual version ----
-## Cov alpha x NEC > 0 ---- 
+# Cov alpha x NEC > 0 ---- 
 alpha = 100
 beta = .08
 Dose = seq(0, 100, by = 10)
@@ -232,7 +69,7 @@ fig.pos.cov.sd = df.pos.cov %>%
   theme(legend.position = "none")
 fig.pos.cov.sd
 
-## Cov alpha x NEC < 0 ----
+# Cov alpha x NEC < 0 ----
 alpha = 100
 beta = .08
 Dose = seq(0, 100, by = 10)
@@ -292,7 +129,7 @@ labs(x = "Dose", y = "sd y response") +
   theme(legend.position = "none")
 fig.neg.cov.sd
 
-## Combine figures ----
+# Combine figures ----
 fig.dr.ID <- fig.pos.cov / fig.neg.cov
 ggsave("outputs/figs/fig.dr.ID.jpeg",
        fig.dr.ID, 
