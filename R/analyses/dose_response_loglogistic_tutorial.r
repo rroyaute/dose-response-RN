819200f91c209aee659d71816284df5a93b37cef
Raphael Royaute <raphael.royaute@inrae.fr>
2026-05-03 16:27:42 +0200

    Split scripts between data simulations, figure plotting and code tutorial

---
+++ R/analyses/dose_response_loglogistic_tutorial.r
@@ -0,0 +1,581 @@
+library(tidyverse)
+# library(drc)
+library(brms)
+library(ggdist)
+library(truncnorm)
+library(distributional)
+library(viridis)
+library(patchwork)
+library(GGally)
+library(tidybayes)
+library(modelr)
+
+## Define brms model ----
+# 3-parameter log-logistic function
+formula_drc <- bf(
+  log(y) ~ log(d / (1 + exp(b * (log(Dose) - log(e))))),
+  b + d + e ~ 1,
+  nl = TRUE
+)
+
+priors <- c(
+  prior(normal(2, 1), nlpar = "b", lb = 0), # slope, positive for decreasing curve
+  prior(normal(0, 0.5), nlpar = "d"), # log(upper asymptote); log(1) = 0 for max y ≈ 1
+  prior(normal(0.5, 0.2), nlpar = "e", lb = 0), # EC50 prior
+  prior(normal(0, 1), class = "sigma")
+)
+
+inits <- list(
+  b_b_Intercept = 2,
+  b_d_Intercept = 1,
+  b_e_Intercept = 0.5
+)
+init_list <- rep(list(inits), 4)
+
+## Plot priors ----
+priors %>%
+  parse_dist() %>%
+  filter(class == "b") %>%
+  ggplot(aes(xdist = .dist_obj, y = format(.dist_obj))) +
+  stat_dist_halfeye() +
+  facet_wrap(~nlpar, scales = "free") +
+  ggtitle("Intercepts") +
+  xlab("Value") +
+  ylab("Density") +
+  theme_bw(12) +
+  theme(axis.text.y = element_text(angle = 90))
+
+## Run model on priors only ----
+brm.drc.prior <- brm(
+  formula = formula_drc,
+  data = df.sim.vp,
+  family = "gaussian",
+  prior = priors,
+  sample_prior = "only",
+  init = rep(list(inits), 4),
+  chains = 4,
+  iter = 2000,
+  seed = 42,
+  backend = "cmdstan",
+  file = "mods/brm.drc.prior",
+  file_refit = "always"
+)
+
+# plot(brm.drc.prior)
+# pp_check(brm.drc.prior, ndraws = 200)
+conditional_effects(brm.drc.prior, method = "posterior_predict")
+conditional_effects(brm.drc.prior, spaghetti = T, ndraws = 100)
+
+## Fit model to simulated data ----
+brm.drc <- brm(
+  formula = formula_drc,
+  data = df.sim.vp,
+  family = "gaussian",
+  prior = priors,
+  sample_prior = "yes",
+  init = rep(list(inits), 4),
+  chains = 4,
+  iter = 4000,
+  control = list(adapt_delta = .95, max_treedepth = 15),
+  seed = 42,
+  backend = "cmdstan",
+  file = "mods/brm.drc",
+  file_refit = "always"
+)
+
+plot(brm.drc)
+pp_check(brm.drc, ndraws = 200)
+conditional_effects(brm.drc, method = "posterior_predict")
+CRI <- as.data.frame(posterior_summary(brm.drc))
+
+## Compute CRI and prediction intervals via posterior_predict ----
+Dose_new <- data.frame(Dose = seq(0, 1, by = 0.01)) %>%
+  mutate(Dose = case_when(Dose == 0 ~ 0.001, .default = Dose))
+
+pred <- posterior_predict(brm.drc, newdata = Dose_new) # Prediction for the expected (mean) value for computing 95% CRI
+epred <- posterior_epred(brm.drc, newdata = Dose_new) # Predictions including residual error to compute 95% Prediction Intervals
+
+pred_df <- data.frame(
+  Dose = Dose_new$Dose,
+  y = apply(pred, 2, function(p) exp(median(p))),
+  y_low = apply(pred, 2, function(p) exp(quantile(p, 0.025))),
+  y_up = apply(pred, 2, function(p) exp(quantile(p, 0.975)))
+)
+
+epred_df <- data.frame(
+  Dose = Dose_new$Dose,
+  y = apply(epred, 2, function(p) exp(median(p))),
+  y_low = apply(epred, 2, function(p) exp(quantile(p, 0.025))),
+  y_up = apply(epred, 2, function(p) exp(quantile(p, 0.975)))
+)
+
+## Plot! ----
+fig_drc_vp.A.up <- ggplot(data = df.sim.vp, aes(x = Dose, y = y)) +
+  # Fitted line
+  geom_line(
+    data = pred_df,
+    aes(x = Dose, y = y),
+    linewidth = 1,
+    color = "dodgerblue"
+  ) +
+  # 95 % Credible Interval for the mean
+  geom_ribbon(
+    data = epred_df,
+    aes(ymin = y_low, ymax = y_up),
+    alpha = .4,
+    fill = "dodgerblue"
+  ) +
+  # 95 % Prediction Interval
+  geom_ribbon(
+    data = pred_df,
+    aes(ymin = y_low, ymax = y_up),
+    alpha = .4,
+    fill = "dodgerblue"
+  ) +
+  # Data points
+  geom_point(
+    shape = 21,
+    colour = "black",
+    fill = "white",
+    size = 2,
+    stroke = 1
+  ) +
+  labs(
+    x = "Dose",
+    y = "Phenotype"
+  ) +
+  theme_custom()
+fig_drc_vp.A
+
+# Figure A: Phenotypic-level - lower panel ----
+## Extract EC50 CI and PI (prediction interval) ----
+# EC50 distribution
+get_variables(brm.drc)
+post_draws.vp <- as_draws_df(brm.drc) %>%
+  select(b_e_Intercept) # Extract posterior distribution for e parameter (EC50)
+# Prediction interval
+pri_lower_bound <- epred_df %>%
+  filter(y_low <= .5)
+pri_lower_bound <- pri_lower_bound[1, ]$Dose # Extract lowest Dose < y = 0.5
+pri_upper_bound <- epred_df %>%
+  filter(y_up <= .5)
+pri_upper_bound <- pri_upper_bound[1, ]$Dose # Extract lowest Dose < y = 0.5
+pri_median <- epred_df %>%
+  filter(y <= .5)
+pri_median <- pri_median[1, ]$Dose # Extract lowest Dose < y = 0.5
+
+df.pri <- data.frame(
+  pri = pri_median,
+  pri_lower_bound = pri_lower_bound,
+  pri_upper_bound = pri_upper_bound
+)
+
+## Plot ----
+# title = "What to report?",
+# subtitle = "Phenotypic variation in EC50"
+
+fig_drc_vp.A.low <- post_draws.vp %>%
+  ggplot(aes(x = b_e_Intercept, y = 0)) +
+  stat_pointinterval() +
+  # geom_linerange(
+  #   data = df.pri,
+  #   aes(y = 1, xmin = pri_lower_bound, xmax = pri_upper_bound)
+  # ) +
+  geom_pointrange(
+    data = df.pri,
+    aes(x = pri, y = 1, xmin = pri_lower_bound, xmax = pri_upper_bound)
+  ) +
+  labs(
+    x = "EC50",
+    y = "Density"
+  ) +
+  theme_custom()
+
+# Figure: Genotype sensitivity ----
+Dose <- seq(0, 1, length.out = 6)
+n_g <- 5 # 5 genotypes
+CVa <- .1 # 10 % of variation around the mean for all parameters
+sigma_d <- d * CVa # Upper bound variation
+sigma_b <- b * CVa # Rate variation
+sigma_e <- e * CVa # EC50 sensitivity variation
+rho <- .4 # moderate correlation among parameters
+# Genotypes with higher basal expression are less sensitive
+# r_dxe = rho: higher basal expression <-> higher EC50
+# r_dxb = -rho : higher basal expression <-> shallower slope
+# r_bxe = -rho : higher EC50 <-> shallower slope
+
+set.seed(42)
+d_g <- rtruncnorm(n_g, mean = d, sd = sigma_d, a = 0)
+b_g <- rtruncnorm(n_g, mean = b, sd = sigma_b, a = 0)
+e_g <- rtruncnorm(n_g, mean = e, sd = sigma_e, a = 0)
+
+Mu <- c(d, b, e)
+sigmas <- c(sigma_d, sigma_b, sigma_e) # 10 % CV around the mean
+names <- c("sigma_d", "sigma_b", "sigma_e")
+rho_mat <- matrix(c(1, -rho, rho, -rho, 1, -rho, rho, -rho, 1), nrow = 3) # Correlation matrix
+colnames(rho_mat) <- names
+rownames(rho_mat) <- names
+Sigma <- diag(sigmas) %*% rho_mat %*% diag(sigmas) # Covariance matrix
+
+set.seed(42)
+G <- MASS::mvrnorm(n_g, Mu, Sigma) %>%
+  data.frame() %>%
+  set_names("d_g", "b_g", "e_g") %>%
+  mutate(G = 1:n_g) %>%
+  arrange(e_g) %>%
+  mutate(color_index = row_number())
+
+# Visualize the among genotype correlations
+G %>%
+  dplyr::select(d_g:e_g) %>%
+  GGally::ggpairs() +
+  theme_bw()
+
+# Visualize genotype-specific dose-responses
+df.sim.lines <- G %>%
+  expand(
+    nesting(G, color_index, d_g, b_g, e_g),
+    Dose = seq(0, 1, by = 0.01)
+  ) %>%
+  mutate(mu = d_g / (1 + exp(b_g * log(Dose / e_g))))
+
+fig_genotypes <- ggplot(
+  df.sim.lines,
+  aes(y = y, x = Dose, color = factor(color_index), fill = factor(color_index))
+) +
+  geom_line(data = df.sim.lines, aes(y = mu, x = Dose), linewidth = 1) +
+  geom_point(
+    data = G,
+    aes(y = d_g / 2, x = e_g),
+    size = 2.5,
+    shape = 21,
+    fill = "white",
+    alpha = .8
+  ) +
+  scale_color_viridis_d(option = "H", direction = -1, begin = .2, end = .8) +
+  # stat_halfeye(aes(y = .5, xdist = dist_normal(e, sigma_e)),
+  #              color = "black", fill = "grey", alpha = .6) +
+  labs(x = "Dose", y = "Phenotype") +
+  theme_bw(12) +
+  theme(legend.position = "none")
+
+ggsave(filename = "outputs/figs/fig_genotypes.jpeg", fig_genotypes)
+
+# Figure: Pre-Post exposure reaction norms ----
+## Dose-response figure ----
+### Simulate data ----
+Dose <- seq(0, 1, length.out = 6)
+n_id <- 20 # 20 individuals per doses
+CVi <- .1 # 10 % of variation around mean for all parameters
+sigma_d <- d * CVi # Upper bound variation
+sigma_b <- b * CVi # Rate variation
+sigma_e <- e * CVi # EC50 sensitivity variation
+rho <- .4 # moderate correlation among parameters
+
+set.seed(42)
+d_i <- rtruncnorm(n_id, mean = d, sd = sigma_d, a = 0)
+b_i <- rtruncnorm(n_id, mean = b, sd = sigma_b, a = 0)
+e_i <- rtruncnorm(n_id, mean = e, sd = sigma_e, a = 0)
+
+Mu <- c(d, b, e)
+sigmas <- c(sigma_d, sigma_b, sigma_e) # 10 % CV around the mean
+rho_mat <- matrix(c(1, -rho, rho, -rho, 1, -rho, rho, -rho, 1), nrow = 3)
+names <- c("sigma_d", "sigma_b", "sigma_e")
+colnames(rho_mat) <- names
+rownames(rho_mat) <- names
+Sigma <- diag(sigmas) %*% rho_mat %*% diag(sigmas) # Covariance matrix
+
+set.seed(42)
+ID <- MASS::mvrnorm(n_id * length(Dose[2:6]), Mu, Sigma) %>%
+  data.frame() %>%
+  set_names("d_i", "b_i", "e_i") %>%
+  mutate(ID = 1:(n_id * length(Dose[2:6]))) %>%
+  mutate(assigned_dose = rep(Dose[2:6], each = n_id)) %>%
+  mutate(control_dose = .001) %>%
+  mutate(Group_n = assigned_dose) %>% # Dose group as numeric
+  mutate(Group_f = as.factor(assigned_dose)) %>% # Dose group as factor
+  arrange(e_i) %>%
+  mutate(color_index = row_number())
+
+# Visualize the among genotype correlations
+ID %>%
+  dplyr::select(d_i:e_i) %>%
+  GGally::ggpairs() +
+  theme_bw()
+
+# Visualize genotype-specific dose-responses
+df.sim.vi <- ID %>%
+  pivot_longer(cols = c(assigned_dose:control_dose), values_to = "Dose") %>%
+  mutate(mu = d_i / (1 + exp(b_i * log(Dose / e_i)))) %>%
+  mutate(y = rlnorm(n(), log(mu), sigma)) %>%
+  mutate(
+    Phase = as.factor(case_when(Dose == 0.001 ~ "Pre", .default = "Post"))
+  ) %>%
+  mutate(Phase = fct_relevel(Phase, "Pre", "Post"))
+
+df.sim.line <- data.frame(Dose = seq(0, 1, by = 0.01)) %>%
+  mutate(y = d / (1 + exp(b * log(Dose / e))))
+
+fig_ID <- ggplot(df.sim.vi, aes(y = y, x = Dose)) +
+  geom_line(linewidth = .5, aes(group = ID), alpha = .15) +
+  geom_point(size = 2.5, shape = 21, fill = "white", alpha = .8) +
+  geom_line(
+    data = df.sim.line,
+    aes(y = y, x = Dose),
+    linewidth = 1,
+    color = "dodgerblue"
+  ) +
+  labs(x = "Dose", y = "Phenotype") +
+  theme_bw(12) +
+  theme(legend.position = "none")
+
+ggsave(filename = "outputs/figs/fig_ID.jpeg", fig_ID)
+
+
+## Among-individual variation in dose-response ----
+### Define brms model ----
+formula_drc_vi <- bf(
+  log(y) ~ log(d / (1 + exp(b * (log(Dose) - log(e))))),
+  b + d + e ~ 1 + (1 | c | ID),
+  nl = TRUE
+)
+
+priors.vi <- c(
+  prior(normal(2, 1), nlpar = "b", lb = 0), # slope, positive for decreasing curve
+  prior(normal(0, 0.5), nlpar = "d"), # log(upper asymptote); log(1) = 0 for max y ≈ 1
+  prior(normal(0.5, 0.2), nlpar = "e", lb = 0), # ED50, unchanged
+  prior(exponential(10), class = "sd", nlpar = "b"), # Exponential prior with mean 0.10
+  prior(exponential(10), class = "sd", nlpar = "d"),
+  prior(exponential(10), class = "sd", nlpar = "e"),
+  prior(exponential(10), class = "sigma"),
+  prior(lkj_corr_cholesky(2), class = "L")
+)
+
+### Plot priors ----
+priors.vi %>%
+  parse_dist() %>%
+  filter(class %in% c("b", "sd", "sigma")) %>%
+  ggplot(aes(xdist = .dist_obj, y = format(.dist_obj))) +
+  stat_dist_halfeye() +
+  facet_wrap(~class, scales = "free") +
+  xlab("Value") +
+  ylab("Density") +
+  theme_bw(12) +
+  theme(axis.text.y = element_text(angle = 90))
+
+### Run model on priors only ----
+brm.drc.vi.prior <- brm(
+  formula = formula_drc_vi,
+  data = df.sim.vi,
+  family = "gaussian",
+  prior = priors.vi,
+  sample_prior = "only",
+  init = rep(list(inits), 4),
+  chains = 4,
+  iter = 4000,
+  threads = threading(4),
+  cores = 4,
+  seed = 42,
+  backend = "cmdstan",
+  file = "mods/brm.drc.prior",
+  file_refit = "always"
+)
+
+# plot(brm.drc.vi.prior)
+conditional_effects(brm.drc.vi.prior, spaghetti = T, ndraws = 100)
+plot(conditional_effects(brm.drc.vi.prior, re_formula = NULL), points = T)
+pp_check(brm.drc.vi.prior, ndraws = 200)
+
+### Fit model to simulated data ----
+brm.drc.vi <- brm(
+  formula = formula_drc_vi,
+  data = df.sim.vi,
+  family = "gaussian",
+  prior = priors.vi,
+  sample_prior = "yes",
+  init = rep(list(inits), 4),
+  chains = 4,
+  warmup = 8000,
+  iter = 10000,
+  control = list(adapt_delta = .95, max_treedepth = 15),
+  threads = threading(4),
+  cores = 4,
+  seed = 42,
+  backend = "cmdstan",
+  file = "mods/brm.drc.vi",
+  file_refit = "always"
+)
+
+# plot(brm.drc.vi)
+plot(
+  conditional_effects(
+    brm.drc.vi,
+    re_formula = NULL,
+    method = "posterior_predict"
+  ),
+  points = T
+)
+pp_check(brm.drc.vi, ndraws = 200)
+
+## Slope variance estimation and figure ----
+### Mean-center data ----
+# Y-values standardized to mean = 0 and SD = 1
+# Within individual centring around pre-post exposure doses (taking mid-point between pre-post exposure)
+df.sim.vi <- df.sim.vi %>%
+  mutate(y_sc = as.numeric(scale(y)))
+
+df.sim.vi.long <- df.sim.vi %>% # long format for comparing model to raw values
+  dplyr::select(ID, Group_n, Group_f, Phase, y, y_sc) %>%
+  pivot_wider(names_from = Phase, values_from = c(y, y_sc))
+
+### Define brms model ----
+formula_lmm_vi <- bf(
+  y_sc ~ Phase * Group_f + (1 + Phase | gr(ID, by = Group_f))
+)
+
+get_prior(formula_lmm_vi, df.sim.vi)
+
+### Prior predictive checks ----
+priors.vi.lmm <- c(
+  prior(normal(0, 1), class = "Intercept"),
+  prior(normal(0, 1), class = "b"),
+  prior(exponential(3), class = "sd", coef = "Intercept", group = "ID"), # Exponential prior with mean .3 on Pre-exposure variation (given y is on SD scale)
+  prior(exponential(5), class = "sd", coef = "PhasePost", group = "ID"), # Exponential prior with mean .2 on slope variation (given y is on SD scale)
+  prior(exponential(3), class = "sigma"), # Exponential prior with mean .3 (given y is on SD scale)
+  prior(lkj_corr_cholesky(2), class = "L")
+)
+
+### Plot priors ----
+priors.vi.lmm %>%
+  parse_dist() %>%
+  filter(class %in% c("b", "sd", "sigma")) %>%
+  ggplot(aes(xdist = .dist_obj, y = format(.dist_obj))) +
+  stat_dist_halfeye() +
+  facet_wrap(~class, scales = "free") +
+  xlab("Value") +
+  ylab("Density") +
+  theme_bw(12) +
+  theme(axis.text.y = element_text(angle = 90))
+
+### Run model on priors only ----
+brm.lmm.vi.prior <- brm(
+  formula = formula_lmm_vi,
+  data = df.sim.vi,
+  family = "gaussian",
+  prior = priors.vi.lmm,
+  sample_prior = "only",
+  chains = 4,
+  iter = 4000,
+  threads = threading(4),
+  cores = 4,
+  seed = 42,
+  backend = "cmdstan",
+  file = "mods/brm.lmm.vi.prior",
+  file_refit = "always"
+)
+
+plot(brm.lmm.vi.prior)
+conditional_effects(brm.lmm.vi.prior, spaghetti = T, ndraws = 100)
+plot(conditional_effects(brm.lmm.vi.prior), points = T)
+pp_check(brm.lmm.vi.prior, ndraws = 200)
+
+### Fit model to simulated data ----
+brm.lmm.vi <- brm(
+  formula = formula_lmm_vi,
+  data = df.sim.vi,
+  family = "gaussian",
+  prior = priors.vi.lmm,
+  sample_prior = "yes",
+  chains = 4,
+  iter = 4000,
+  # control = list(adapt_delta = .95, max_treedepth = 15),
+  threads = threading(4),
+  cores = 4,
+  seed = 42,
+  backend = "cmdstan",
+  file = "mods/brm.lmm.vi",
+  file_refit = "always"
+)
+
+plot(brm.lmm.vi)
+plot(
+  conditional_effects(
+    brm.lmm.vi,
+    # re_formula = NULL,
+    method = "posterior_predict"
+  ),
+  points = T
+)
+pp_check(brm.lmm.vi, ndraws = 500)
+
+
+### Extract slope variance at each dose ----
+brm.lmm.vi <- readRDS("mods/brm.lmm.vi.rds")
+post_draws.vi <- as_draws_df(brm.lmm.vi)
+post_draws.vi <- post_draws.vi %>%
+  dplyr::select(
+    `sd_ID__PhasePost:Group_f0.2`,
+    `sd_ID__PhasePost:Group_f0.4`,
+    `sd_ID__PhasePost:Group_f0.6`,
+    `sd_ID__PhasePost:Group_f0.8`,
+    `sd_ID__PhasePost:Group_f1`
+  ) %>%
+  pivot_longer(
+    cols = starts_with("sd"),
+    names_to = 'Treatment',
+    names_prefix = "sigma_i",
+    values_to = "Estimate"
+  ) %>%
+  mutate(
+    Treatment = case_when(
+      str_detect(Treatment, "Group_f0.2") == T ~ .2,
+      str_detect(Treatment, "Group_f0.4") == T ~ .4,
+      str_detect(Treatment, "Group_f0.6") == T ~ .6,
+      str_detect(Treatment, "Group_f0.8") == T ~ .8,
+      str_detect(Treatment, "Group_f1") == T ~ 1
+    )
+  )
+fig_slope_vi <- post_draws.vi %>%
+  ggplot(aes(y = Estimate, x = Treatment)) +
+  stat_halfeye() +
+  theme_bw() +
+  theme(legend.position = "none")
+# Compare to simulated values
+df.sim.vi.long %>%
+  mutate(
+    slope_i = (y_Post - y_Pre) / (Group_n / 2),
+    slope_i_sc = (y_sc_Post - y_sc_Pre) / (Group_n / 2)
+  ) %>%
+  summarise(
+    slope_mu = mean(slope_i),
+    slope_sd = sd(slope_i),
+    slope_sd_sc = sd(slope_i_sc),
+    .by = c(Group_n)
+  ) %>%
+  arrange(Group_n) %>%
+  ggplot(aes(x = Group_n, y = slope_sd_sc)) +
+  geom_point()
+
+
+### Replicate fig_ID on model predicted values (TODO) ----
+get_variables(brm.lmm.vi)
+newdata <- df.sim.vi
+
+Slope_dist <- brm.lmm.vi %>%
+  epred_draws(newdata, re_formula = NULL)
+
+Slope_dist_mqi <- brm.lmm.vi %>%
+  epred_draws(newdata, re_formula = NULL) %>%
+  median_qi() %>%
+  mutate(
+    Dose = case_when(Phase == "Pre" ~ 0.001, .default = Dose_sc_i * 2),
+    y = .epred * sd(df.sim.vi$y) + mean(df.sim.vi$y)
+  )
+
+fig_ID_mod <- ggplot(Slope_dist_mqi, aes(x = Dose, y = y, group = ID)) +
+  geom_line(linewidth = .5, aes(group = ID), alpha = .15) +
+  geom_point(size = 2.5, shape = 21, fill = "white", alpha = .8) +
+  theme_bw()
+
+((fig_ID / fig_ID_mod) & ylim(0, 1.6) & xlim(0, 1.2)) /
+  (fig_slope_vi & xlim(0, 1.2))
